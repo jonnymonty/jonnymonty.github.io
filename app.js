9bc5626712449def17a54014aac61703defec066
Jonny Monty <jonny_monty11@hotmail.com>
2020-06-16 04:15:23 -0500

    Redesigned contact and projects page

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.nav-links li');
-const logoname = document.querySelector('.name h1');
+const logoname = document.querySelector('.logo');
 const displayProjectContainer = document.querySelector('.displayProjectContainer');
 
 // TO:DO add the ability to click Jonny Monty to take you to the about page
